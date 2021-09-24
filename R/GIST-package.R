c1759bf5f1896b06d5537d86a8fcf8df73a652e4
asif zubair <asif.zubair@gmail.com>
2021-09-24 10:52:03 -0500

    initial commit

---
+++ R/GIST-package.R
@@ -0,0 +1,16 @@
+#' The 'GIST' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name GIST-package
+#' @aliases GIST
+#' @useDynLib GIST, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.21.2. https://mc-stan.org
+#'
+NULL
