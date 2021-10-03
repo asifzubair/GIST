7e4784c6f002eae568d93334e8fc8e4be68468dc
Asif <asif.zubair@gmail.com>
2021-10-02 22:00:44 -0500

    GIST

---
+++ R/GIST.R
@@ -0,0 +1,64 @@
+#' Guided Imae Spatial Transcriptomics
+#'
+#' #' @param st_expression spatial transcriptomics expression matrix (genes by spot)
+#' @param sig_mat signature matrix (genes by celltype)
+#' @param prior_values
+#' @param prior_index index in signature matrix of cell type which needs a prior on it
+#' @param prior_lambda
+#' @param ... arguments to be passed to \code{rstan::sampling} (e.g. \code{chains, iter, init, verbose, refresh})
+#' @return An object of type dataframe with posterior mean estimates
+#' @export
+GIST <- function(st_expression, sig_mat, prior_values = NULL, prior_index = NULL,  prior_lambda = 50, num_cores = 1, ...){
+
+  # TODO: this following sanity check is very basic,
+  # TODO: need to handle situations in which either row or column vectors are passed
+  if (is.numeric(st_expression)) st_expression <- as.data.frame(st_expression)
+
+  genes <- intersect(rownames(sig_mat), rownames(st_expression))
+  message('Overalp betweeb ST and SC genes:', length(genes), '\n')
+
+  st_expression <- st_expression[genes, ]
+  sig_mat <- sig_mat[genes, ]
+  stopifnot(all(rownames(sig_mat) == rownames(st_expression)))
+
+  numGenes = nrow(st_expression)
+  numCellTypes = ncol(sig_mat)
+
+  #TODO: ideally, I want to do some diagnostic checks based on Stan warnings.
+  #TODO: will probably write a curried function that does diagnostic checks.
+  if (is.null(prior_index))
+    model = stanmodels$GIST_base_model
+  else
+    model = stanmodels$GIST_enhanced_model
+
+  ## get kwargs and register parallel backend
+  args0 = list(...)
+  if (num_cores > 1){
+    cl <- parallel::makeCluster(num_cores)
+    doParallel::registerDoParallel(cl)
+  }
+
+  parout <- foreach::foreach(i = seq_len(NCOL(st_expression)),
+                             .packages = "rstan",
+                             .combine = rbind) %dopar%  {
+      if (is.null(prior_index))
+        standata <- list(numGenes = numGenes, numCellTypes = numCellTypes,
+                         exprMixVec = st_expression[, i], sigMat = sig_mat)
+      else
+        standata <- list(numGenes = numGenes, numCellTypes = numCellTypes,
+                         exprMixVec = st_expression[, i], sigMat = sig_mat,
+                         prior_phi = prior_values[i], prior_index = prior_index, prior_lambda = prior_lambda)
+
+      nmfOut <- do.call(sampling, c(list(object = model, data = standata), args0))
+      stanSumNmf <- extract(nmfOut)$estimatedProportionsVecSimp[, 1:numCellTypes]
+      pEstimatesList <- colMeans(stanSumNmf)
+
+      return(pEstimatesList)
+    }
+
+  parallel::stopCluster(cl)
+
+  colnames(parout) <- colnames(sig_mat)
+  rownames(parout) <- colnames(st_expression)
+  return(parout)
+}
